e645e114eda17d31cb3edfa87fc9b65f64c9edb0
Jolene Kearse <70668540+JoleneKearse@users.noreply.github.com>
2022-04-24 17:33:30 -0400

    prevented reverse move on mobile

--- input.js
+++ input.js
@@ -31,17 +31,30 @@ window.addEventListener("keydown", e => {
 button.forEach(ele => {
     ele.addEventListener("click", function handleClick(event) {
         if (ele.classList.contains("up")) {
-            console.log("up clicked")
-            inputDirection = {x: 0, y: -1}
+            if (lastInputDirection.y == 1) {
+                inputDirection = {x: 0, y: 0}
+            } else {
+                inputDirection = {x: 0, y: -1}
+            }
         } else if (ele.classList.contains("down")) {
-            console.log("down clicked")
+            if (lastInputDirection.y == -1) {
+                inputDirection = {x: 0, y: 0}
+            } else {
+                inputDirection = {x: 0, y: 1}
+            }
             inputDirection = {x: 0, y: 1}
         } else if (ele.classList.contains("left")) {
-            console.log("left clicked")
-            inputDirection = {x: -1, y: 0}
+            if (lastInputDirection.x == 1) {
+                inputDirection = {x: 0, y: 0}
+            } else {
+                inputDirection = {x: -1, y: 0}
+            }
         } else if (ele.classList.contains("right")) {
-            console.log("right clicked")
-            inputDirection = {x: 1, y: 0}
+            if (lastInputDirection.x == -1) {
+                inputDirection = {x: 0, y: 0}
+            } else {
+                inputDirection = {x: 1, y: 0}
+            }
         }
     })
 })
